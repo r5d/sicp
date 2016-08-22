3188380b871a4a18f5a12e3d52a8e0243954dcf2
rsiddharth <s@ricketyspace.net>
2016-08-22 03:00:05 +0000

    one/eleven.scm: update header comment styling.

--- one/eleven.scm
+++ one/eleven.scm
@@ -1,6 +1,6 @@
-;;; Under Creative Commons Attribution-ShareAlike 4.0
-;;; International. See
-;;; <https://creativecommons.org/licenses/by-sa/4.0/>.
+;;;; Under Creative Commons Attribution-ShareAlike 4.0
+;;;; International. See
+;;;; <https://creativecommons.org/licenses/by-sa/4.0/>.
 
 (define-module (one eleven)
   #:export (recursive-fn))
