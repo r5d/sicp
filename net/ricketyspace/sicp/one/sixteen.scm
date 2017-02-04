b947a6167b51546a3deed1e99de33f10884e389e
rsiddharth <s@ricketyspace.net>
2017-02-04 02:32:46 +0000

    Add net/ricketyspace/sicp/one/sixteen.scm

---
+++ net/ricketyspace/sicp/one/sixteen.scm
@@ -0,0 +1,26 @@
+;;;; Under Creative Commons Attribution-ShareAlike 4.0
+;;;; International. See
+;;;; <https://creativecommons.org/licenses/by-sa/4.0/>.
+
+;;;  _____________________________________
+;;; < successive squaring 𝚯(log n) drivel >
+;;;  -------------------------------------
+;;;     \
+;;;      \
+;;;     ^__^         /
+;;;     (oo)\_______/  _________
+;;;     (__)\       )=(  ____|_ \_____
+;;;         ||----w |  \ \     \_____ |
+;;;         ||     ||   ||           ||
+;;;
+
+(define-module (net ricketyspace sicp one sixteen)
+  #:export (fast-expt-iter))
+
+(define (sqr x) (expt x 2))
+
+(define (fast-expt-iter b n a)
+  "Excercise 1.16."
+  (cond ((= n 0) a)
+        ((even? n) (fast-expt-iter b (- n 2) (* a (sqr b))))
+        (else (fast-expt-iter b (- n 1) (* a b)))))
