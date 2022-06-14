4ce415d7264d8b6db516a5572815d8d5ffaa3227
siddharth ravikumar <s@ricketyspace.net>
2022-06-13 20:30:56 -0400

    ex: 50.rkt: implement counterclockwise-180

--- ex/2/50.rkt
+++ ex/2/50.rkt
@@ -16,14 +16,23 @@
                   (sub-vect (m corner1) new-origin)
                   (sub-vect (m corner2) new-origin)))))))
 
+
 (define (flip-vert painter)
   (transform-painter painter
                      (make-vect 0.0 1.0)
                      (make-vect 1.0 1.0)
                      (make-vect 0.0 0.0)))
 
+
 (define (flip-horiz painter)
   (transform-painter painter
                      (make-vect 1.0 0.0)
                      (make-vect 0.0 0.0)
                      (make-vect 1.0 1.0)))
+
+
+(define (counterclockwise-180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
