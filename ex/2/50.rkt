9dfe891335881717fb06e20707389464c82b05d3
siddharth ravikumar <s@ricketyspace.net>
2022-06-13 20:31:40 -0400

    ex: 50.rkt: implement counterclockwise-270

--- ex/2/50.rkt
+++ ex/2/50.rkt
@@ -36,3 +36,10 @@
                      (make-vect 1.0 1.0)
                      (make-vect 0.0 1.0)
                      (make-vect 1.0 0.0)))
+
+
+(define (counterclockwise-270 painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
